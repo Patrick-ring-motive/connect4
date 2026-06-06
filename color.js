410bc9f424fbf6b6c32a020fd3ca2570dcc6c163
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-05 20:38:00 -0500

    Create color.js

---
+++ color.js
@@ -0,0 +1,12 @@
+
+const curlyRegex = /[\{\}‘’']/;
+const squareRegex = /[\[\]“”""]/;
+const pRegex = /[\(\)]/;
+const symRegex = /[^a-zA-Z0-9\s\[\]\{\}\(\)“”"‘’'"]+/g;
+const numRegex = /[0-9]+/;
+const yRegex = /\bY\b/;
+const rRegex = /\bR\b/;
+
+function color(text){
+  
+}
