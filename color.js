5ff444f72aae4a70f1a2541accecf6581ded1745
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-07 06:27:07 -0500

    Update color.js

--- color.js
+++ color.js
@@ -8,31 +8,40 @@
     }
   };
   const regexes = {
-    star:[/(\bx\b)|\*+/,'cornflowerblue'],
-    curly: [/[\{\}‘’']/, '#ff79c6'],
-    square: [/[\[\]“”""]/, '#ba7dff'],
-    paren: [/[\(\)]/, 'orange'],
-    symbol: [/[^a-zA-Z0-9\s\[\]\{\}\(\)“”"‘’'"\*]+/, '#00ff00'],
+    star:[/\*+/,'cornflowerblue'],
+    curly: [/[\{\}‘’']+/, '#ff79c6'],
+    square: [/[\[\]“”""]+/, '#ba7dff'],
+    paren: [/[\(\)]+/, 'orange'],
     number: [/[0-9]+/, 'deepskyblue'],
-    yellow: [/\b(Y|Yellow)\b/, 'yellow'],
-    red: [/\b(R|Red)\b/, 'red']
   };
-  const allRegex = re(Object.values(regexes).map(x => x[0].source).join('|'), 'ig');
-  const textShadow = 'black';
-  const shadowSize ='0.1ch';
+  const compoundRe = {
+    yellow: [/\b(Y|Yellow)\b/, 'yellow'],
+    red: [/\b(R|Red)\b/, 'red'],
+    x:[/(\bx\b)/,'#ffffff'],
+    symbol: [re(`[^a-zA-Z0-9\s${Object.values(regexes).map(x => x[0].source.slice(1,-2)).join('')}]+`), '#00ff00'],
+    ...regexes
+  }
+  const allRegex = re(Object.values(compoundRe).map(x => x[0].source).join('|'), 'ig');
+  const ts = 'black';
+  const sz ='0.1ch';
   let bold = false;
   globalThis.color =  (text) => 
     text.replace(allRegex,ch => {
-      for (const key in regexes) {
-        if (regexes[key][0].test(ch)) {
+      for (const key in compoundRe) {
+        if (compoundRe[key][0].test(ch)) {
           return `<span class="color-${key}">${ch.replace(/[<>]/g,'^')}</span>`;
         }
       }
       return ch;
     });
   const style = document.createElement('style');
-  for (const key in regexes) {
-    style.textContent += ` .color-${key} { color: ${regexes[key][1]} !important; text-shadow: -${shadowSize} -${shadowSize} 0 ${textShadow}, ${shadowSize} -${shadowSize} 0 ${textShadow}, -${shadowSize} ${shadowSize} 0 ${textShadow}, ${shadowSize} ${shadowSize} 0 ${textShadow} !important; } `;
+  for (const key in compoundRe) {
+    style.textContent += ` .color-${key} ${
+      JSON.stringify({
+        color: `${compoundRe[key][1]} !important`,
+        "text-shadow": `-${sz} -${sz} 0 ${sz}, ${sz} -${sz} 0 ${ts}, -${sz} ${sz} 0 ${ts}, ${sz} ${sz} 0 ${ts} !important`,
+      },null,2).replaceAll('"','').replace(/!important,?/g,'!important;')
+    } `;
   }
   document.firstElementChild.appendChild(style);
 })();
