297bc5bdbbb7238a8167f0f3bcd2d51cbb27776d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-11 08:58:47 -0500

    style: js-beautify formatting (color.js)

--- color.js
+++ color.js
@@ -1,17 +1,21 @@
 (() => {
-  const {keys,values,entries} = Object;
+  const {
+    keys,
+    values,
+    entries
+  } = Object;
   const isString = x => typeof x === 'string' || x instanceof String;
   const isObject = x => typeof x === 'object' && x !== null;
   const toArray = x => x?.[Symbol.iterator] ? [...x] : (isObject(x) ? entries(x) : [...String(x)]);
-  const stringify = (...args) =>{
+  const stringify = (...args) => {
     const x = args[0];
-    try{
-      if(isString(x)){
+    try {
+      if (isString(x)) {
         return String(x);
       }
       return String(JSON.stringify(...args));
-    }catch(e){
-      console.warn(e,...args.map(Boolean));
+    } catch (e) {
+      console.warn(e, ...args.map(Boolean));
       return String(x);
     }
   };
@@ -23,12 +27,12 @@
       return RegExp();
     }
   };
-  const g = x => re(x,[...new Set(`${x?.flags||''}g`)].join``);
-  const _g = x => re(x,String(x?.flags||'').replaceAll('g',''));
+  const g = x => re(x, [...new Set(`${x?.flags||''}g`)].join``);
+  const _g = x => re(x, String(x?.flags || '').replaceAll('g', ''));
   const rm = (x, y) => String(x).replaceAll(g(y), '');
   const split = (x, y) => String(x).split(_g(y));
-  const join = (x=[],y='')=>[...(x[Symbol.iterator]?x:toArray(x))].map(stringify).join(String(y));
-  const test = (x,str)=> re(x).test(stringify(str));
+  const join = (x = [], y = '') => [...(x[Symbol.iterator] ? x : toArray(x))].map(stringify).join(String(y));
+  const test = (x, str) => re(x).test(stringify(str));
   const regexes = {
     star: [/[\*•]+/, 'cornflowerblue'],
     curly: [/[\{\}‘’']+/, '#ff79c6'],
@@ -43,13 +47,13 @@
     symbol: [re(`[^a-zA-Z0-9\s${join(values(regexes).map(x =>x[0].source.slice(1,-2)))}]+`), '#00ff00'],
     ...regexes
   };
-  const allRegex = re(join(values(compoundRe).map(x => x[0].source),'|'), 'ig');
+  const allRegex = re(join(values(compoundRe).map(x => x[0].source), '|'), 'ig');
   const ts = 'black';
   const sz = '0.1ch';
   globalThis.color = (text) =>
     text.replace(allRegex, ch => {
       for (const key in compoundRe) {
-        if (test(compoundRe[key]?.[0],ch)) {
+        if (test(compoundRe[key]?.[0], ch)) {
           return `<span class="color-${key}">${rm(ch,/[<>]/g)}</span>`;
         }
       }
@@ -62,6 +66,6 @@
         color: `${compoundRe[key]?.[1]} !important`,
         "text-shadow": `-${sz} -${sz} 0 ${sz}, ${sz} -${sz} 0 ${ts}, -${sz} ${sz} 0 ${ts}, ${sz} ${sz} 0 ${ts} !important`,
       },null,2),'"').replace(/!important,?/g,'!important;')
-    }`),' ');
+    }`), ' ');
   document.firstElementChild.appendChild(style);
 })();
